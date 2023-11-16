6c1e0263ccc85b6c9672beb9560171e52b091f19
Sahith Akula <akulasahith268@gmail.com>
2023-11-16 19:21:15 +0530

    Some more changes wre added

--- GameLogic.java
+++ GameLogic.java
@@ -9,15 +9,21 @@ public class GameLogic {
                 board[i][j] = ' ';
             }
         }
+        int count = 0;
         boolean gameover = false;
         char player = 'X';
         while(!gameover){
             printboard(board);
-            System.out.println("Player" + "  "+ player + "enter row and column");
+            if(count==9){
+                System.out.println("Game is Drawn");
+                break;
+            }
+            System.out.println("Player" + "  "+ player + " "+"enter row and column");
             int row = sc.nextInt();
             int column = sc.nextInt();
             if(board[row][column]==' '){
                 board[row][column] = player;
+                count++;
                 gameover = IsWin(player, board);
                 if(gameover){
                     System.out.println("Player"+" " + player + "is win");
@@ -60,11 +66,18 @@ public class GameLogic {
     }
 
     public static void printboard(char[][] board) {
+        System.out.println("---------------");
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j] + " | ");
+                if(j==2){
+                     System.out.print(" | " + board[i][j] + " | ");
+                }
+                else{
+                System.out.print(" | " + board[i][j]);
+                }
             }
             System.out.println();
+            System.out.println("---------------");
         }
     }
 }
